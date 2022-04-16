6a707a85f0eb4a808d3c631e46d9be2234534cbb
danalexandru <danb.alexandru@gmail.com>
2022-04-16 21:38:56 +0300

    add: Added the "Capscolor" patch
    reference: https://tools.suckless.org/slock/patches/capscolor/

--- config.h
+++ config.h
@@ -3,9 +3,10 @@ static const char *user  = "";
 static const char *group = "";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT]      =   "#000000",     /* after initialization */
+	[INPUT]     =   "#005577",   /* during input */
+	[FAILED]    =   "#CC3333",   /* wrong password */
+	[CAPS]      =   "#E7C547",         /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
